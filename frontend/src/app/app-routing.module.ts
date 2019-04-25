df5d6f7643925604fb91cbad1a7f5c34776ba576
gabsoft <gabriel.almeida-save@outlook.com.br>
2019-04-25 13:32:54 -0300

    finalizacao do layout mockado, inicio da criacao de apis no wordpress

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ParquesComponent } from './parques/parques.component'
+import { FestasComponent } from './festas/festas.component'
+import { HomeComponent } from './home/home.component'
+import { ContatoComponent } from './contato/contato.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: HomeComponent},
+  { path: 'parques', component: ParquesComponent },
+  { path: 'festas', component: FestasComponent },
+  { path: 'contato', component: ContatoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ParquesComponent } from './parques/parques.component';
+import { FestasComponent } from './festas/festas.component';
+import { ContatoComponent } from './contato/contato.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ParquesComponent,
+    FestasComponent,
+    ContatoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
